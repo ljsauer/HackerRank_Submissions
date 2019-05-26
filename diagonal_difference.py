efc7b6b7c0a52adbb37ff0916dfba2c5d9e764bb
ljsauer <48888021+ljsauer@users.noreply.github.com>
2019-05-26 01:47:16 -0500

    Create diagonal_difference.py
    Function that takes a square matrix as input and gives the output of its diagonals' absolute difference.

---
+++ diagonal_difference.py
@@ -0,0 +1,14 @@
+# Purpose: Given a square matrix, return the absolute
+# difference of the sums of its diagonals.
+
+
+def diagonalDifference(arr):
+    diag_one = 0
+    diag_two = 0
+    j = len(arr) - 1
+    for i in range(len(arr)):
+        diag_one += arr[i][i]
+        diag_two += arr[i][j]
+        j -= 1
+    abs_diff = abs(diag_one - diag_two)
+    return abs_diff
